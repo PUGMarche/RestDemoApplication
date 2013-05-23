c06d47cf1b566fd4fc20f1db4419890d4dd1158c
Francesco Tassi <tassi.francesco@gmail.com>
2013-05-23 18:59:20 +0200

    RED - Implemento test per creazione nuovo customer

--- src/PugM/RestDemoBundle/Tests/Controller/CustomerControllerTest.php
+++ src/PugM/RestDemoBundle/Tests/Controller/CustomerControllerTest.php
@@ -37,4 +37,31 @@ class CustomerControllerTest extends WebTestCase{
         $this->assertEquals('foocustomer@example.org', $response['email']);
         $this->assertEquals('logo.jpg', $response['logo']);
     }
+
+    public function testPostCustomer()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/customer', array(), array(), array(
+                'HTTP_ACCEPT' => 'application/json'
+            ),
+            '{"name": "New customer name","address": "New customer adress","email": "newcustomer@example.org","logo": "logo.jpg"}'
+        );
+
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+
+        $client->request('GET', $client->getResponse()->headers->get('Location'), array(), array(), array(
+                'HTTP_ACCEPT' => 'application/json'
+            ));
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('application/json', $client->getResponse()->headers->get('Content-Type'));
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertEquals(2, $response['id']);
+        $this->assertEquals('New customer name', $response['name']);
+        $this->assertEquals('New customer adress', $response['address']);
+        $this->assertEquals('newcustomer@example.org', $response['email']);
+        $this->assertEquals('logo.jpg', $response['logo']);
+    }
 }
